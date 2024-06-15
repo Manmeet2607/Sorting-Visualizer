c971d5690937f25e9a336357e0cfa3c476ed05ba
Manmeet2607 <jaisikasaini@icloud.com>
2024-06-15 15:36:36 +0530

    v

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from './components/Home/Home';
 import Sorting from './components/Sorting/Sorting.jsx'
 
@@ -9,7 +8,7 @@ import Sorting from './components/Sorting/Sorting.jsx'
 function App() {
   return (
     <div className="App">
-      <BrowserRouter  basename='/Sorting-Visualizer'>
+      <BrowserRouter>
       <Routes>
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/sorting" element={<Sorting/>} />
